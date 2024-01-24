a0c969483ee1b05c7039d37c9fd816cee889fdec
bradiepoa7@gmail.com <bradiepoa7@gmail.com>
2024-01-24 18:41:38 +0300

    my email slicer perfector

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 while True:
-    # ceating a valiable
+    # creating a variable
     email = input('Enter an email to slice: ')
+    
     # finding an index, letter you want
     # print(email.index('@'))
-    if email.count('@')== 1 and length(email[:email.index('@')]) and length(email[email.index('@')+1:]):
+    
+    if email.count('@') == 1 and len(email[:email.index('@')]) > 0 and len(email[email.index('@')+1:]) > 0:
         print("Your username is : " + email[:email.index('@')])
         print("And your domain name is : " + email[email.index('@')+1:])
     else:
